af736dcc7cb7cf1d6d73ef33eafe3a1dc9c1ff08
Paul Bailey <paul@neutron.studio>
2021-08-17 14:59:08 -0500

    heroku deploy

--- warpzone/settings/base.py
+++ warpzone/settings/base.py
@@ -135,7 +135,7 @@ AUTH_USER_MODEL = 'account.User'
 
 REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379')
 if REDIS_URL.startswith('redis://'):
-    redis_connection = {'url': REDIS_URL '/0'}
+    redis_connection = {'url': REDIS_URL + '/0'}
 
 else:
     redis_url = urlparse(os.environ.get("REDIS_URL"))
